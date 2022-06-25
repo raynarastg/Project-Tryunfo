3a13731b5c5be4319060c66f42622630be8d0f52
Raynara Santiago <raysantiagobz@gmail.com>
2022-06-24 23:40:23 -0300

    faz validação do botão

--- src/App.js
+++ src/App.js
@@ -14,16 +14,40 @@ class App extends React.Component {
       image: '',
       rare: '',
       trunfo: '',
+      button: true,
     };
   }
 
+  validacao = () => {
+    const { name,
+      description, attr1, attr2, attr3, image, rare } = this.state;
+    const valorMax = 210;
+    const pontosMax = 90;
+    const soma = Number(attr1) + Number(attr2) + Number(attr3);
+    this.setState({
+      button: !(name !== ''
+        && description !== ''
+        && image !== ''
+        && rare !== ''
+        && attr1 >= 0
+        && attr2 >= 0
+        && attr3 >= 0
+        && attr1 <= pontosMax
+        && attr2 <= pontosMax
+        && attr3 <= pontosMax
+        && soma <= valorMax) });
+  }
+
+  // nessa parte foi utilizado métodos que estão nesta documentação https://pt-br.reactjs.org/docs/react-component.html#setstate
   onInputChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState(({ [name]: value }), () => this.validacao());
   }
 
   render() {
-    const { name, description, attr1, attr2, attr3, image, rare, trunfo } = this.state;
+    const {
+      name,
+      description, attr1, attr2, attr3, image, rare, trunfo, button } = this.state;
     return (
       <div>
         <Form
@@ -36,6 +60,8 @@ class App extends React.Component {
           cardRare={ rare }
           cardTrunfo={ trunfo }
           onInputChange={ this.onInputChange }
+          // onSaveButtonClick={this.validacao}
+          isSaveButtonDisabled={ button }
         />
         <Card
           cardName={ name }
